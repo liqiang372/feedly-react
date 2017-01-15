6154c0608926db35b1c0048962627d6d84e0c371
belfast <liqiang372@gmail.com>
2017-01-14 17:28:03 -0800

    add semantic-ui-react

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,17 +1,40 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import styles from './Home.css';
+import { Sidebar, Segment, Menu, Header, Icon, Button} from 'semantic-ui-react'
+import "./Home.css"
 
-
+const styles = {
+  sideBar: {
+    'width': '200px',
+    'border': 'none',
+    'borderRadius': 'none'
+  },
+  addNew: {
+    'fontSize': '24px',
+    'padding': '5px'
+  }
+}
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <div className={styles.container}>
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
-        </div>
+      <div style={{"height": "100vh"}}>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar as={Menu} animation='push' visible={true}
+                  icon='labeled' vertical
+                  className="f-sidebar">
+            <Menu.Item name='addNew'>
+              <Button fluid style={styles.addNew}>+</Button>
+            </Menu.Item>
+
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Segment basic>
+              <Header as='h3'>hello there</Header>
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+
       </div>
     );
   }
